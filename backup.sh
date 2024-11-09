655b610522ac4762e52aa91b5f26dcc44c119257
vlanty <74422614+vlanty@users.noreply.github.com>
2024-11-09 14:18:40 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -52,6 +52,39 @@ while true; do
     fi
 done
 
+
+# x-ui or marzban or hiddify
+# گرفتن نوع نرم افزاری که می‌خواهیم پشتیبانی از آن بگیریم و ذخیره آن در متغیر xmh
+while [[ -z "$xmh" ]]; do
+    echo "x-ui or marzban or hiddify? [x/m/h] : "
+    read -r xmh
+    if [[ $xmh == $'\0' ]]; then
+        echo "Invalid input. Please choose x, m or h."
+        unset xmh
+    elif [[ ! $xmh =~ ^[xmh]$ ]]; then
+        echo "${xmh} is not a valid option. Please choose x, m or h."
+        unset xmh
+    fi
+done
+
+while [[ -z "$crontabs" ]]; do
+    echo "Would you like the previous crontabs to be cleared? [y/n] : "
+    read -r crontabs
+    if [[ $crontabs == $'\0' ]]; then
+        echo "Invalid input. Please choose y or n."
+        unset crontabs
+    elif [[ ! $crontabs =~ ^[yn]$ ]]; then
+        echo "${crontabs} is not a valid option. Please choose y or n."
+        unset crontabs
+    fi
+done
+
+if [[ "$crontabs" == "y" ]]; then
+# remove cronjobs
+sudo crontab -l | grep -vE '/root/ac-backup.+\.sh' | crontab -
+fi
+
+
 # m backup
 # ساخت فایل پشتیبانی برای نرم‌افزار Marzban و ذخیره آن در فایل ac-backup.zip
 if [[ "$xmh" == "m" ]]; then
@@ -67,11 +100,7 @@ if [ -d "/var/lib/marzban/mysql" ]; then
 
   sed -i -e 's/\s*=\s*/=/' -e 's/\s*:\s*/:/' -e 's/^\s*//' /opt/marzban/.env
 
-  # MySQL container name: mysql
-  MYSQL_CONTAINER_NAME="mysql"
-  
-  # Backup script for MySQL database inside Docker container
-  docker exec $MYSQL_CONTAINER_NAME bash -c "mkdir -p /var/lib/mysql/db-backup"
+  docker exec marzban-mysql-1 bash -c "mkdir -p /var/lib/mysql/db-backup"
   source /opt/marzban/.env
 
     cat > "/var/lib/marzban/mysql/ac-backup.sh" <<EOL
@@ -80,36 +109,105 @@ if [ -d "/var/lib/marzban/mysql" ]; then
 USER="root"
 PASSWORD="$MYSQL_ROOT_PASSWORD"
 
+
 databases=\$(mysql -h 127.0.0.1 --user=\$USER --password=\$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
 
 for db in \$databases; do
     if [[ "\$db" != "information_schema" ]] && [[ "\$db" != "mysql" ]] && [[ "\$db" != "performance_schema" ]] && [[ "\$db" != "sys" ]] ; then
         echo "Dumping database: \$db"
-        mysqldump -h 127.0.0.1 --force --opt --user=\$USER --password=\$PASSWORD --databases \$db > /var/lib/mysql/db-backup/\$db.sql
+		mysqldump -h 127.0.0.1 --force --opt --user=\$USER --password=\$PASSWORD --databases \$db > /var/lib/mysql/db-backup/\$db.sql
+
     fi
 done
+
 EOL
 chmod +x /var/lib/marzban/mysql/ac-backup.sh
 
 ZIP=$(cat <<EOF
-docker exec $MYSQL_CONTAINER_NAME bash -c "/var/lib/mysql/ac-backup.sh"
+docker exec marzban-mysql-1 bash -c "/var/lib/mysql/ac-backup.sh"
 zip -r /root/ac-backup-m.zip /opt/marzban/* /var/lib/marzban/* /opt/marzban/.env -x /var/lib/marzban/mysql/\*
 zip -r /root/ac-backup-m.zip /var/lib/marzban/mysql/db-backup/*
 rm -rf /var/lib/marzban/mysql/db-backup/*
 EOF
 )
 
-else
-  ZIP="zip -r /root/ac-backup-m.zip ${dir}/* /var/lib/marzban/* /opt/marzban/.env"
+    else
+      ZIP="zip -r /root/ac-backup-m.zip ${dir}/* /var/lib/marzban/* /opt/marzban/.env"
 fi
 
 ACLover="marzban backup"
 
-# Add to telegram and cronjob
+# x-ui backup
+# ساخت فایل پشتیبانی برای نرم‌افزار X-UI و ذخیره آن در فایل ac-backup.zip
+elif [[ "$xmh" == "x" ]]; then
+
+if dbDir=$(find /etc /opt/freedom -type d -iname "x-ui*" -print -quit); then
+  echo "The folder exists at $dbDir"
+  if [[ $dbDir == *"/opt/freedom/x-ui"* ]]; then
+     dbDir="${dbDir}/db/"
+  fi
+else
+  echo "The folder does not exist."
+  exit 1
+fi
+
+if configDir=$(find /usr/local -type d -iname "x-ui*" -print -quit); then
+  echo "The folder exists at $configDir"
+else
+  echo "The folder does not exist."
+  exit 1
+fi
+
+ZIP="zip /root/ac-backup-x.zip ${dbDir}/x-ui.db ${configDir}/config.json"
+ACLover="x-ui backup"
+
+# hiddify backup
+# ساخت فایل پشتیبانی برای نرم‌افزار Hiddify و ذخیره آن در فایل ac-backup.zip
+elif [[ "$xmh" == "h" ]]; then
+
+if ! find /opt/hiddify-manager/hiddify-panel/ -type d -iname "backup" -print -quit; then
+  echo "The folder does not exist."
+  exit 1
+fi
+
+ZIP=$(cat <<EOF
+cd /opt/hiddify-manager/hiddify-panel/
+if [ $(find /opt/hiddify-manager/hiddify-panel/backup -type f | wc -l) -gt 100 ]; then
+  find /opt/hiddify-manager/hiddify-panel/backup -type f -delete
+fi
+python3 -m hiddifypanel backup
+cd /opt/hiddify-manager/hiddify-panel/backup
+latest_file=\$(ls -t *.json | head -n1)
+rm -f /root/ac-backup-h.zip
+zip /root/ac-backup-h.zip /opt/hiddify-manager/hiddify-panel/backup/\$latest_file
+
+EOF
+)
+ACLover="hiddify backup"
+else
+echo "Please choose m or x or h only !"
+exit 1
+fi
+
+
+trim() {
+    # remove leading and trailing whitespace/lines
+    local var="$*"
+    # remove leading whitespace characters
+    var="${var#"${var%%[![:space:]]*}"}"
+    # remove trailing whitespace characters
+    var="${var%"${var##*[![:space:]]}"}"
+    echo -n "$var"
+}
+
 IP=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
 caption="${caption}\n\n${ACLover}\n<code>${IP}</code>\nCreated by @AC_Lover - https://github.com/AC-Lover/backup"
 comment=$(echo -e "$caption" | sed 's/<code>//g;s/<\/code>//g')
 comment=$(trim "$comment")
+
+# install zip
+# نصب پکیج zip
+sudo apt install zip -y
 
 # send backup to telegram
 # ارسال فایل پشتیبانی به تلگرام
@@ -119,6 +217,7 @@ $ZIP
 echo -e "$comment" | zip -z /root/ac-backup-${xmh}.zip
 curl -F chat_id="${chatid}" -F caption=\$'${caption}' -F parse_mode="HTML" -F document=@"/root/ac-backup-${xmh}.zip" https://api.telegram.org/bot${tk}/sendDocument
 EOL
+
 
 # Add cronjob
 # افزودن کرانجاب جدید برای اجرای دوره‌ای این اسکریپت
