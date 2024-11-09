c06fbcfa3ba3950b361c322ded709a1e96cc394c
vlanty <74422614+vlanty@users.noreply.github.com>
2024-11-09 14:08:03 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Получение токена бота от пользователя
+# Bot token
+# گرفتن توکن ربات از کاربر و ذخیره آن در متغیر tk
 while [[ -z "$tk" ]]; do
     echo "Bot token: "
     read -r tk
@@ -10,7 +11,8 @@ while [[ -z "$tk" ]]; do
     fi
 done
 
-# Получение chat ID
+# Chat id
+# گرفتن Chat ID از کاربر و ذخیره آن در متغیر chatid
 while [[ -z "$chatid" ]]; do
     echo "Chat id: "
     read -r chatid
@@ -23,11 +25,13 @@ while [[ -z "$chatid" ]]; do
     fi
 done
 
-# Заголовок для резервной копии
-echo "Caption (for example, your domain, to identify the backup more easily): "
+# Caption
+# گرفتن عنوان برای فایل پشتیبان و ذخیره آن در متغیر caption
+echo "Caption (for example, your domain, to identify the database file more easily): "
 read -r caption
 
-# Настройка cronjob для регулярного выполнения
+# Cronjob
+# تعیین زمانی برای اجرای این اسکریپت به صورت دوره‌ای
 while true; do
     echo "Cronjob (minutes and hours) (e.g : 30 6 or 0 12) : "
     read -r minute hour
@@ -48,47 +52,82 @@ while true; do
     fi
 done
 
-# Пути к директориям для резервного копирования
-directories_to_backup=(
-    "/opt/marzban"
-    "/var/lib/marzban"
-    "/opt/marzban/.env"
+# m backup
+# ساخت فایل پشتیبانی برای نرم‌افزار Marzban و ذخیره آن در فایل ac-backup.zip
+if [[ "$xmh" == "m" ]]; then
+
+if dir=$(find /opt /root -type d -iname "marzban" -print -quit); then
+  echo "The folder exists at $dir"
+else
+  echo "The folder does not exist."
+  exit 1
+fi
+
+if [ -d "/var/lib/marzban/mysql" ]; then
+
+  sed -i -e 's/\s*=\s*/=/' -e 's/\s*:\s*/:/' -e 's/^\s*//' /opt/marzban/.env
+
+  # MySQL container name: mysql
+  MYSQL_CONTAINER_NAME="mysql"
+  
+  # Backup script for MySQL database inside Docker container
+  docker exec $MYSQL_CONTAINER_NAME bash -c "mkdir -p /var/lib/mysql/db-backup"
+  source /opt/marzban/.env
+
+    cat > "/var/lib/marzban/mysql/ac-backup.sh" <<EOL
+#!/bin/bash
+
+USER="root"
+PASSWORD="$MYSQL_ROOT_PASSWORD"
+
+databases=\$(mysql -h 127.0.0.1 --user=\$USER --password=\$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
+
+for db in \$databases; do
+    if [[ "\$db" != "information_schema" ]] && [[ "\$db" != "mysql" ]] && [[ "\$db" != "performance_schema" ]] && [[ "\$db" != "sys" ]] ; then
+        echo "Dumping database: \$db"
+        mysqldump -h 127.0.0.1 --force --opt --user=\$USER --password=\$PASSWORD --databases \$db > /var/lib/mysql/db-backup/\$db.sql
+    fi
+done
+EOL
+chmod +x /var/lib/marzban/mysql/ac-backup.sh
+
+ZIP=$(cat <<EOF
+docker exec $MYSQL_CONTAINER_NAME bash -c "/var/lib/mysql/ac-backup.sh"
+zip -r /root/ac-backup-m.zip /opt/marzban/* /var/lib/marzban/* /opt/marzban/.env -x /var/lib/marzban/mysql/\*
+zip -r /root/ac-backup-m.zip /var/lib/marzban/mysql/db-backup/*
+rm -rf /var/lib/marzban/mysql/db-backup/*
+EOF
 )
 
-# Путь к данным MySQL в Docker
-mysql_data_path="/var/lib/docker/volumes/mysql/_data"
-
-# Проверка наличия директории с данными MySQL
-if [ -d "$mysql_data_path" ]; then
-    echo "MySQL data directory found at $mysql_data_path"
-    
-    # Создаем резервную копию всех данных MySQL через mysqldump внутри контейнера
-    docker exec -t mysql-container-name mysqldump -u root -pYOUR_PASSWORD --all-databases > /root/mysql_backup.sql
-
-    # Архивируем директорию с данными MySQL
-    backup_filename="/root/mysql_backup_$(date +'%Y%m%d_%H%M%S').tar.gz"
-    tar -czf "$backup_filename" -C "$mysql_data_path" .
-
-    # Добавляем другие директории в архив
-    backup_filename_all="/root/marzban_backup_$(date +'%Y%m%d_%H%M%S').tar.gz"
-    tar -czf "$backup_filename_all" "${directories_to_backup[@]}" /root/mysql_backup.sql
-
-    # Отправка архивного файла в Telegram
-    curl -X POST "https://api.telegram.org/bot$tk/sendDocument" \
-        -F chat_id=$chatid \
-        -F document=@"$backup_filename_all" \
-        -F caption="$caption"
-
-    # Сохранение архива в локальном каталоге для дальнейших копий
-    local_backup_path="/home/backups/marzban_backup_$(date +'%Y%m%d_%H%M%S').tar.gz"
-    cp "$backup_filename_all" "$local_backup_path"
-
-    # Опционально: Удаление старых архивов (например, более 30 дней)
-    find /home/backups/ -type f -name "marzban_backup_*.tar.gz" -mtime +30 -exec rm {} \;
-
-    # Уведомление о завершении
-    echo "Backup completed and sent to Telegram. Local backup saved at $local_backup_path."
 else
-    echo "MySQL data directory not found at $mysql_data_path!"
-    exit 1
+  ZIP="zip -r /root/ac-backup-m.zip ${dir}/* /var/lib/marzban/* /opt/marzban/.env"
 fi
+
+ACLover="marzban backup"
+
+# Add to telegram and cronjob
+IP=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
+caption="${caption}\n\n${ACLover}\n<code>${IP}</code>\nCreated by @AC_Lover - https://github.com/AC-Lover/backup"
+comment=$(echo -e "$caption" | sed 's/<code>//g;s/<\/code>//g')
+comment=$(trim "$comment")
+
+# send backup to telegram
+# ارسال فایل پشتیبانی به تلگرام
+cat > "/root/ac-backup-${xmh}.sh" <<EOL
+rm -rf /root/ac-backup-${xmh}.zip
+$ZIP
+echo -e "$comment" | zip -z /root/ac-backup-${xmh}.zip
+curl -F chat_id="${chatid}" -F caption=\$'${caption}' -F parse_mode="HTML" -F document=@"/root/ac-backup-${xmh}.zip" https://api.telegram.org/bot${tk}/sendDocument
+EOL
+
+# Add cronjob
+# افزودن کرانجاب جدید برای اجرای دوره‌ای این اسکریپت
+{ crontab -l -u root; echo "${cron_time} /bin/bash /root/ac-backup-${xmh}.sh >/dev/null 2>&1"; } | crontab -u root -
+
+# run the script
+# اجرای این اسکریپت
+bash "/root/ac-backup-${xmh}.sh"
+
+# Done
+# پایان اجرای اسکریپت
+echo -e "\nDone\n"
